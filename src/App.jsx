3180cfbd3f2834ac9795b56ce98a1075635ea725
soulnishk0 <nikakupavtseva88@gmail.com>
2023-06-03 00:45:43 +0300

    Add label

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,17 @@ import { CustomizationProvider } from "./contexts/Customization";
 
 function App() {
   return (
-    <CustomizationProvider>
-      <div className="App">
-        <Canvas dpr={[1, 2]}>
-          <color attach="background" args={["#213547"]} />
-          <fog attach="fog" args={["#213547", 10, 20]} />
-          <Experience />
-        </Canvas>
-        <Configurator />
-      </div>
-    </CustomizationProvider>
+      <CustomizationProvider>
+        <div className="App">
+          <div className="label">Bento Bakehouse</div> {/* Add the label here */}
+          <Canvas dpr={[1, 2]}>
+            <color attach="background" args={["#5a3072"]} />
+            <fog attach="fog" args={["#5a3072", 10, 20]} />
+            <Experience />
+          </Canvas>
+          <Configurator />
+        </div>
+      </CustomizationProvider>
   );
 }
 
